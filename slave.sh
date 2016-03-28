baf41d6e2e3669fd1d6aaaa3c6a9ce93cb74362e
Albert Strasheim <albert@mesosphere.com>
2016-03-28 00:02:12 -0700

    fix slave cgroups

--- slave.sh
+++ slave.sh
@@ -1,2 +1,18 @@
 #!/bin/bash -xe
-sudo systemd-nspawn -D slave -b --bind-ro=/usr/bin/docker:/usr/bin/docker --bind=/var/run/docker.sock:/var/run/docker.sock --bind-ro=/bin/true:/usr/sbin/modprobe --bind=/home:/home --network-bridge=docker0
+sudo mkdir -p /sys/fs/cgroup/cpu,cpuacct/machine.slice/machine-slave.scope
+sudo mkdir -p /sys/fs/cgroup/memory/machine.slice/machine-slave.scope
+sudo mkdir -p /sys/fs/cgroup/freezer/machine.slice/machine-slave.scope
+sudo mkdir -p /sys/fs/cgroup/net_cls,net_prio/machine.slice/machine-slave.scope
+sudo mkdir -p /sys/fs/cgroup/perf_event/machine.slice/machine-slave.scope
+sudo systemd-nspawn \
+	-D slave \
+	-b --bind-ro=/usr/bin/docker:/usr/bin/docker \
+	--bind=/var/run/docker.sock:/var/run/docker.sock \
+	--bind-ro=/bin/true:/usr/sbin/modprobe \
+	--bind=/home:/home \
+	--bind /sys/fs/cgroup/cpu,cpuacct/machine.slice/machine-slave.scope:/sys/fs/cgroup/cpu,cpuacct \
+	--bind /sys/fs/cgroup/memory/machine.slice/machine-slave.scope:/sys/fs/cgroup/memory \
+	--bind /sys/fs/cgroup/freezer/machine.slice/machine-slave.scope:/sys/fs/cgroup/freezer \
+	--bind /sys/fs/cgroup/net_cls,net_prio/machine.slice/machine-slave.scope:/sys/fs/cgroup/net_cls,net_prio \
+	--bind /sys/fs/cgroup/perf_event/machine.slice/machine-slave.scope:/sys/fs/cgroup/perf_event \
+	--network-bridge=docker0
