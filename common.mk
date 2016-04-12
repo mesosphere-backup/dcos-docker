f0293349f39c7c2c1cd40c5080cf9b389c595f33
Jess Frazelle <jess@mesosphere.com>
2016-04-11 18:33:49 -0700

    fix help
    Signed-off-by: Jess Frazelle <jess@mesosphere.com>

--- common.mk
+++ common.mk
@@ -22,7 +22,7 @@ REGISTRY_HOST := registry.local
 
 .PHONY: help
 help: ## Generate the Makefile help
-	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' Makefile | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
 .PHONY: ips
 ips: ## Gets the ips for the currently running containers.
