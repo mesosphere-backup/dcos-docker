a87c8638edecf23a6ec3a0c15da53f3f167d87b3
Karl Isenberg <karl@mesosphere.io>
2017-05-26 20:56:34 -0700

    Remove unused teamcity files
    - move useful extract script to ci/

--- ci/dcos-extract.sh
+++ ci/dcos-extract.sh
@@ -1,5 +1,16 @@
-#!/bin/bash -xe
+#!/usr/bin/env bash
+
+# Extracts the release files from a DC/OS installer (dcos_generate_config.sh).
+#
+# Usage:
+# $ ci/dcos-extract.sh [installer-path]
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
 DCOS_GENERATE_CONFIG="${1:-dcos_generate_config.sh}"
+
 sudo rm -rf tmp
 mkdir -p tmp
 sed '0,/^#EOF#$/d' $DCOS_GENERATE_CONFIG | tar Oxv | tar -xC tmp
