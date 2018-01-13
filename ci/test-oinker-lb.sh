50a9ea563091534b5214779682fa00f2ad483bb8
Karl Isenberg <karl@mesosphere.io>
2018-01-13 12:51:30 -0800

    Add sequential successful requests requirement to oinker LB test
    - This should cut down the number of flakey failures by waiting
      until the responses are settled and successful.

--- ci/test-oinker-lb.sh
+++ ci/test-oinker-lb.sh
@@ -30,10 +30,11 @@ echo >&2  "Looking up app (${APP_ID}) instances..."
 INSTANCES="$(dcos marathon app show "${APP_ID}" | jq '.instances')"
 
 # Test load balancing uses all instances
+echo "Waiting 30s for successful responses from ${INSTANCES} distinct tasks..."
 declare -A TASK_IDS=()
 START_TIME=${SECONDS}
 while [[ ${#TASK_IDS[@]} -lt ${INSTANCES} ]]; do
-  echo "Polling http://${OINKER_HOST}/..."
+  echo "Polling..."
   if RESPONSE_BODY="$(curl --fail --location --silent --show-error "http://${OINKER_HOST}/")"; then
     TASK_ID="$(echo "${RESPONSE_BODY}" | grep '<div.*>oinker.*</div>' | sed 's/.*\(oinker\.[^<]*\).*/\1/')"
     echo "TASK_ID: ${TASK_ID}"
@@ -51,4 +52,27 @@ while [[ ${#TASK_IDS[@]} -lt ${INSTANCES} ]]; do
     exit 1
   fi
 done
-echo >&2 "Tasks seen:" "${#TASK_IDS[@]}"
+echo >&2 "Distinct tasks: ${#TASK_IDS[@]}"
+
+# Require sequential successful results to indicate stability
+EXPECTED=${INSTANCES}
+let EXPECTED*=5
+echo "Waiting 60s for ${EXPECTED} successful sequential responses..."
+COUNTER=0
+START_TIME=${SECONDS}
+while [[ ${COUNTER} -lt ${EXPECTED} ]]; do
+  echo "Polling..."
+  if RESPONSE_BODY="$(curl --fail --location --silent --show-error "http://${OINKER_HOST}/")"; then
+    let COUNTER+=1
+  else
+    echo "Resetting counter (was ${COUNTER})"
+    COUNTER=0
+  fi
+
+  ELAPSED_TIME=$((${SECONDS} - ${START_TIME}))
+  if [[ ${ELAPSED_TIME} -gt 60 ]]; then
+    echo >&2 "Load balancing failure -- Timed out after 60 seconds."
+    exit 1
+  fi
+done
+echo >&2 "Successful sequential responses: ${COUNTER}"
